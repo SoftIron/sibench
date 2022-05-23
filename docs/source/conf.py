6c8d9198ff7cac951c91bd57701be5cfd82a97dc
Pedro Alvarez <pedro.alvarez@softiron.com>
2022-05-23 16:07:07 +0100

    Enable autosecitonlabel and prefix the document
    This will help us creating links from one page to another

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,10 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  'sphinx.ext.autosectionlabel',
 ]
+
+autosectionlabel_prefix_document = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
